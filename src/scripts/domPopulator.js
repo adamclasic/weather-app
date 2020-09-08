9e273aa18b208f8c9a6ad214daefccebb02efdcd
adam allalou <aabou9@gmail.com>
2020-09-08 11:19:27 +0100

    fix background image issue

--- src/scripts/domPopulator.js
+++ src/scripts/domPopulator.js
@@ -3,6 +3,32 @@ import rainGif from '../assets/images/rain.gif';
 import clearPng from '../assets/images/clear-sky.png';
 import cloudsGif from '../assets/images/clouds.gif';
 import snowGif from '../assets/images/snow.gif';
+
+function setBackgroundImage( status ) {
+  let url = '';
+  switch (status) {
+    case 'Rain':
+      url = rainGif;
+      break;
+    case 'Thunderstorm':
+      url = rainGif;
+      break;
+    case 'Drizzle':
+      url = rainGif;
+      break;
+    case 'Clear':
+      url = clearPng;
+      break;
+    case 'Snow':
+      url = snowGif;
+      break;
+    default:
+      url = cloudsGif;
+      break;
+  }
+  return url;
+}
+
 export default function domPopulator(object) {
   let weatherBox = document.querySelector('.weather-box')
   let cityName = weatherBox.querySelector('.city-name');
@@ -13,7 +39,7 @@ export default function domPopulator(object) {
   let tempMax = weatherBox.querySelector('#tempMax');
   let wind = weatherBox.querySelector('#wind');
   let body = document.querySelector('body');
-
+  let url = setBackgroundImage(object.weather[0].main)
   cityName.innerHTML = object.name;
   tempCels.innerHTML = object.main.temp + ' C';
   tempIcon.setAttribute('src', "http://openweathermap.org/img/wn/" + object.weather[0].icon + ".png");
@@ -21,27 +47,5 @@ export default function domPopulator(object) {
   tempMin.innerHTML = `Min temp: ${object.main.temp_min}` + ' C';
   tempMax.innerHTML = `Max temp: ${object.main.temp_max}` + ' C';
   wind.innerHTML = `Wind speed: ${object.wind.speed}` + ' Km/h';
-
-  let url = '';
-
-  switch (object.weather[0].main) {
-    case 'Rain':
-      // url = 'url(./assets/images/rain.gif)'
-      url = rainGif;
-      break;
-    case 'shower rain':
-      url = rainGif;
-      break;
-    case 'clear sky':
-      url = clearPng;
-      break;
-    case 'snow':
-      url = snowGif;
-      break;
-    default:
-      url = cloudsGif;
-      break;
-  }
-console.log(rainGif);
   body.style.backgroundImage = `url(${url})`;
 }
