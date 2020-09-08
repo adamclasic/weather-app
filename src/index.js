5de3fb79bde2b50b81421ca4063b238b2015a05e
adam allalou <aabou9@gmail.com>
2020-09-08 12:31:05 +0100

    add metric select to html

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './styles/style.scss';
 import { getTemp } from './scripts/getTemp';
 import domPopulator from './scripts/domPopulator';
 
+// console.log(https://api.openweathermap.org/data/2.5/weather?q=paris&appid=98f5c39a38b987172eb484d62acb0f9c&units=metric`)
+
 getTemp().then(domPopulator);
+getTemp('paris').then(console.log);
 
 const searchBtn = document.querySelector('form');
 searchBtn.addEventListener('submit', (e) => {
