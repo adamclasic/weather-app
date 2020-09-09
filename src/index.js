05f97d0a4a218cfe24eeb9b94366d84f829261d3
adam allalou <aabou9@gmail.com>
2020-09-09 11:50:13 +0100

    update alert

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-alert, import/no-extraneous-dependencies */
+/* eslint-disable import/no-extraneous-dependencies */
 import 'bootstrap';
 import './styles/style.scss';
 import { getTemp } from './scripts/getTemp';
@@ -7,6 +7,8 @@ import domPopulator from './scripts/domPopulator';
 getTemp().then(domPopulator);
 
 const searchBtn = document.querySelector('form');
+const toggleBtn = document.querySelector('#toggleFC');
+
 searchBtn.addEventListener('submit', (e) => {
   const searchValue = document.querySelector('#searchInput').value;
   const metricValue = document.querySelector('#inputMetric').value;
@@ -16,9 +18,24 @@ searchBtn.addEventListener('submit', (e) => {
       domPopulator(res, metricValue);
     })
     .catch(() => {
-      alert('location you entered is not valid.');
-      getTemp().then((res) => {
-        domPopulator(res, metricValue);
-      });
+      // alert('location you entered is not valid.');
+      document.querySelector('.alert').classList.add('show')
+      setTimeout(() => {
+      document.querySelector('.alert').classList.remove('show')
+      }, 3000)
+      document.querySelector('#searchInput').value = '';
+      getTemp().then(domPopulator);
     });
+});
+
+toggleBtn.addEventListener('click', () => {
+  console.log('hi');
+  const searchValue = document.querySelector('#searchInput').value;
+  let metricValue = document.querySelector('#inputMetric').value;
+  metricValue = metricValue === 'Metric' ? 'Imperial' : 'Metric';
+  document.querySelector('#inputMetric').value = metricValue;
+  console.log(metricValue);
+  getTemp(searchValue, metricValue).then((res) => {
+    domPopulator(res, metricValue)
+  });
 });
--- src/scripts/getTemp.js
+++ src/scripts/getTemp.js
@@ -15,6 +15,7 @@ async function getTemp(location = null, metricValue = 'Metric') {
   if (!location) {
     const ip = await getIp();
     location = await getLocation(ip);
+    location.trim();
   }
   const temp = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=98f5c39a38b987172eb484d62acb0f9c&units=${metricValue}`);
   const data = await temp.json();
