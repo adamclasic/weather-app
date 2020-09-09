f351ab91f898684babae9e8281eb7c7aa8dc2f14
adam allalou <aabou9@gmail.com>
2020-09-09 09:01:08 +0100

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,17 +8,17 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.(png|svg|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-            },
-          },
-        ],
-      },
+      // {
+      //   test: /\.(png|svg|jpg|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: 'url-loader',
+      //       options: {
+      //         limit: 8192,
+      //       },
+      //     },
+      //   ],
+      // },
       {
         test: /\.scss$/,
         use: [
